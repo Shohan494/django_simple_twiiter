c893d1aada39d54702d3bd92e628281148f2292c
shohan494 <shohan12224943.gmail.com>
2016-06-07 12:13:29 +0600

    created migration migrated, edited the urls and views

---
+++ tweet/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+
+from .views import *
+
+urlpatterns = [
+    url(r'^sign/up/$', sign_up, name='sign_up'),
+    url(r'^sign/up/process/$', sign_up_process, name='sign_up_process'),
+    url(r'^sign/up/success/$', sign_up_success, name='sign_up_success'),
+
+    url(r'^sign/in/$', sign_in, name='sign_in'),
+    url(r'^sign/in/process/$', sign_in_process, name='sign_in_process'),
+
+    url(r'^sign/out/$', log_out, name='logout')
+]
--- tweet/views.py
+++ tweet/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth import login, logout
+
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+
+def sign_up(request):
+    form = UserCreationForm()
+    return render(request, 'user/sign/up.html', {'form': form})
+
+
+def sign_up_process(request):
+    new_user_form = UserCreationForm(request.POST or None)
+    if new_user_form.is_valid():
+        new_user_form.save()
+        return redirect(reverse('sign_up_success'))
+    return render(request, 'user/sign/up.html', {'form': new_user_form})
+
+
+def sign_up_success(request):
+    return render(request, 'user/sign/up_success.html')
+
+
+def sign_in(request):
+    form = AuthenticationForm()
+    return render(request, 'user/sign/in.html', {'form': form})
+
+
+def sign_in_process(request):
+    form = AuthenticationForm(data=request.POST or None)
+    if form.is_valid():
+        login(request, form.get_user())
+        return redirect('/')
+    return render(request, 'user/sign/in.html', {'form': form})
+
+
+def log_out(request):
+    logout(request)
+    return redirect('/')
